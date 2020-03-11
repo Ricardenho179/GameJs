17599b054d63ff7ff815a55c4a906dd38210704d
Ricardo Araújo <39573274+Ricardenho179@users.noreply.github.com>
2020-03-11 11:17:14 -0300

    Melhoria de layout e movimentacao dos objetos

--- ProjetoNv2/index.js
+++ ProjetoNv2/index.js
@@ -1,4 +1,4 @@
-alert("Corrida de quadrados")
+alert("Descubra as funcionalidades de cada quadrado")
 function startGame() {
     myGameArea.start();
     GamePiece1 = new component(15, 15, "grey", 10, 10);
@@ -35,6 +35,13 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        //inserindo comando por teclado
+        window.addEventListener('keydown', function (e) {
+          myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+          myGameArea.key = false;
+        })
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -46,27 +53,59 @@ function updateGameArea() {
     myGameArea.clear();
     myGamePiece.newPos();
     myGamePiece.update();
-    GamePiece1.x += 1.5;
+    GamePiece1.newPos();
     GamePiece1.update();
-    GamePiece2.x += 3;
-    GamePiece2.y += 3;
+    GamePiece2.speedX = 0;
+    GamePiece2.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 65) {GamePiece2.speedX = -2; }
+    if (myGameArea.key && myGameArea.key == 68) {GamePiece2.speedX = 2; }
+    if (myGameArea.key && myGameArea.key == 87) {GamePiece2.speedY = -2; }
+    if (myGameArea.key && myGameArea.key == 83) {GamePiece2.speedY = 2; }
+    GamePiece2.newPos();
     GamePiece2.update();
     GamePiece3.x += 1.9;
     GamePiece3.update();
   }
-  function moveup() {
+  //CIMA
+  function moveupBlackPiece() {
     myGamePiece.speedY -= 1;
   }
-  
-  function movedown() {
+  function moveUpGreyPiece() {
+    GamePiece1.speedY -=1;
+  }
+  //BAIXO
+  function moveDownBlackPiece() {
     myGamePiece.speedY += 1;
   }
-  
-  function moveleft() {
+  function moveDownGreyPiece() {
+    GamePiece1.speedY +=1;
+  }
+  // GamePiece2.speedY +=1;
+  // GamePiece3.speedY +=1;
+  //Esquerda
+  function moveLeftBlackPiece() {
     myGamePiece.speedX -= 1;
   }
-  
-  function moveright() {
-    myGamePiece.speedX += 1;
+  function moveLeftGreyPiece() {
+    GamePiece1.speedX -= 1;
   }
-
+  // GamePiece2.speedX -= 1;
+  // GamePiece3.speedX -= 1;
+  //Direita
+  function moveRightBlackPiece() {
+    myGamePiece.speedX += 1;  
+  }
+  function moveRightGreyPiece() {
+  GamePiece1.speedX += 1;
+  }
+  // GamePiece2.speedX += 1;
+  // GamePiece3.speedX += 1;
+  //Stop
+  function stopMove() {
+    GamePiece1.speedX = 0;
+    GamePiece1.speedY = 0;
+    GamePiece2.speedX = 0;
+    GamePiece2.speedY = 0;
+    GamePiece3.speedX = 0;
+    GamePiece3.speedY = 0;
+  }
